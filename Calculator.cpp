78b2a8fda49335c1b2270f4001e9d92d93e51ac7
Dev. Sean Ken <DevSean2001@gmail.com>
2025-08-26 15:45:08 +0800

    Added the logic and source code for the terminal calculator prototype. Not yet finish

---
+++ Calculator.cpp
@@ -0,0 +1,86 @@
+// Copyright (c) 2025 DevKid2001
+// MIT License
+
+#include <iostream>
+#include <string>
+#include <limits>
+
+void Calculator() {
+
+    std::cout << "1. Addition" << "\n";
+    std::cout << "2. Subtraction" << "\n";
+    std::cout << "3. Multiplication" << "\n";
+    std::cout << "4. Division" << "\n";
+
+    std::string operation;
+
+    while (true) {
+        std::cout << "Enter what operation do you want to perform: ";
+        std::getline(std::cin, operation);
+
+        if (operation.empty()) {
+            std::cout << "[+]Error: Cannot Be Empty." << "\n";
+        } else {
+            break;
+        }
+    }
+
+
+    if (operation == "1" || operation == "2" || operation == "3" || operation == "4") {
+        int num1 = 0;
+
+        while (true) {
+            std::cout << "Enter first number: ";
+            std::cin >> num1;
+
+            if (std::cin.fail()) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "[+]Error: " << num1 << " is possible not a number." << "\n";
+            } else {
+                break;
+            }
+        }
+
+        int num2 = 0;
+
+        while (true) {
+            std::cout << "Enter second number: ";
+            std::cin >> num2;
+
+            if (std::cin.fail()) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>:: max(), '\n');
+                std::cout << "[+]Error: " << num2 << " is possible not a number." << "\n";
+            } else {
+                break;
+            }
+        }
+
+        if (operation == "1") {
+            int add = num1 + num2;
+            std::cout << "Result: " << add << "\n";
+        } else if (operation == "2") {
+            int minus = num1 - num2;
+            std::cout << "Result: " << minus << "\n";
+        } else if (operation == "3") {
+            int times = num1 * num2;
+            std::cout << "Result: " << times << "\n";
+        } else if (operation == "4") {
+            if (num2 == 0) {
+                std::cout << num1 << " cannot be diveded by 0" << "\n";
+            } else {
+                int divide = num1 / num2;
+                std::cout << "Result: " << divide << "\n";
+            }
+        }
+    }
+
+}
+
+int main() {
+
+    Calculator();
+
+    return 0;
+}
